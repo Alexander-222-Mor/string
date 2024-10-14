6ccfcbf6502506dd54b959efba0feadc033bdac1
Alexander-222-Mor <amorgunov948@gmail.com>
2024-10-14 22:32:58 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,10 @@
+example = "Python"
+print (example [0])
+example = "Python"
+print (example [-1])
+example = "Python"
+print (example [-3:])
+example = "Python"
+print (example [::-1])
+example = "Python"
+print (example [1:6:2])
