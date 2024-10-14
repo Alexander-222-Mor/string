021d3855d1f201d16635900cffc81e2c2460f1ad
Alexander-222-Mor <amorgunov948@gmail.com>
2024-10-14 22:39:07 +0300

    Update string.py

--- string.py
+++ string.py
@@ -1,10 +1,6 @@
 example = "Python"
 print (example [0])
-example = "Python"
 print (example [-1])
-example = "Python"
 print (example [-3:])
-example = "Python"
 print (example [::-1])
-example = "Python"
 print (example [1:6:2])
